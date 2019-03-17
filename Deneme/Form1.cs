282a2d9db0b874aaa63a3c781bad3dfee46e031b
Test <Test@TEST-PC>
2019-03-17 20:51:45 +0100

    ok

---
+++ Deneme/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string Mypass = "We are no longer working on older versions of OSForensics, but you can download the older versions of OSForensics on";
+
+        }
+    }
+}
